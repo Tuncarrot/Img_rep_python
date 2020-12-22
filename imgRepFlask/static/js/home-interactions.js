7dbe1bedb205b76f9d64ea0749e1b89a0c52b015
AdamTunkiewicz <adastunkiewicz@gmail.com>
2020-12-21 23:44:34 -0500

    :lipstick: update styling to reflect project direction

---
+++ imgRepFlask/static/js/home-interactions.js
@@ -0,0 +1,12 @@
+function switchLayout() {
+    var icon = document.getElementById("layout-icon");
+    var para = document.getElementById("layout-paragraph");
+    if (icon.style.display === "none") {
+        icon.style.display = "block";
+        para.style.display = "none";
+    } else {
+        icon.style.display = "none";
+        para.style.display = "block";
+    }
+  } 
+
