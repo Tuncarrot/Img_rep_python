570f0e50c96082cc1a1feb3615e54565453cb8f6
Adam Tunkiewicz <Atunkiewicz0881@conestogac.on.ca>
2021-01-09 13:32:07 -0500

    :sparkles: add new routes for album and picture features

--- imgRepFlask/routes.py
+++ imgRepFlask/routes.py
@@ -1,51 +1,53 @@
-import os, binascii
+
+from __future__ import print_function
+import os, binascii, sys
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request # import render_template function for rendering HTML pages individually, url 4 for finding files in the background
 from imgRepFlask import app, db, bcrypt, constants
-from imgRepFlask.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from imgRepFlask.forms import RegistrationForm, LoginForm, UpdateAccountForm, UploadPhotoForm
 from imgRepFlask.models import User, Account, ContactInfo, Album, Picture                          # Using models in views, DB needs to exist first
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author':'blah',
-        'title':'post 1',
-        'content':'first post',
-        'date_created':'today'
-    },
-    {
-        'author':'blah2',
-        'title':'post 21',
-        'content':'second post',
-        'date_created':'today'
-    }
-]
 
 @app.route('/')             # routes
 @app.route('/home')            
 def home_page():
-    return render_template('home.html', title='Welcome', posts = posts)
+    return render_template('home.html', title='Welcome')
 
 @app.route('/about')         
 def about_page():
     return render_template('about.html', title='About')
 
-@app.route('/browse')            
+@app.route('/browse')
+@login_required
 def browse_page():
-    return render_template('browse.html', title='Welcome', posts = posts)
+    images = list()
+    albums = Album.query.filter_by(user_id=current_user.id).all()
+    print(albums, sep='\n', file=sys.stderr)
+    if not albums:
+        flash('There are currently no albums to view, add an album first!', 'danger')
+        return redirect(url_for('home_page'))
+    else:
+        for album in albums:
+            photos = Picture.query.filter_by(album_id=album.id).first()
+            images.append(photos)
+            print(images, sep='\n', file=sys.stderr)
+        return render_template('browse.html', title='Browse Photos', photos = images)
 
-@app.route('/register', methods=['GET', 'POST'])         
+
+
+@app.route('/register', methods=['GET', 'POST'])       
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home_page'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        contactInfo = ContactInfo(name=form.name.data)
-        account = Account(email=form.email.data, password=hashed_password)
         user = User() #album is not required to make user
         #user = User(name=form.name.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
+        db.session.commit()
+        contactInfo = ContactInfo(name=form.name.data, creator=user)
+        account = Account(email=form.email.data, password=hashed_password, creator=user)
         db.session.add(contactInfo)
         db.session.add(account)
         db.session.commit()
@@ -88,11 +90,14 @@ def save_picture(form_picture): #Move this into seperate file, along with db que
 def account_updateInfo():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        if form.picture.data:youtube
+        user_account = Account.query.filter_by(id=current_user.id).first()
+        user_contact = ContactInfo.query.filter_by(id=current_user.id).first()
+        if form.picture.data:
             picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.name = form.name.data
-        current_user.email = form.email.data
+            user_account.image_file = picture_file
+            #current_user.image_file = picture_file
+        user_contact.name = form.name.data
+        user_account.email = form.email.data
         db.session.commit()
         flash('Account Details Updated', 'success')
         return redirect(url_for('account_home'))
@@ -108,10 +113,76 @@ def account_updateInfo():
 @login_required         
 def account_home():
     form = UpdateAccountForm()
+    user_account = Account.query.filter_by(id=current_user.id).first()
+    user_contact = ContactInfo.query.filter_by(id=current_user.id).first()
     if request.method == 'GET':
-        form.name.data = ContactInfo.name
-        form.email.data = Account.email
-    image_file= url_for('static', filename='profile_pics/' + Account.image_file)          
+        form.name.data = user_contact.name
+        form.email.data = user_account.email
+    image_file= url_for('static', filename='profile_pics/' + user_account.image_file)          
     return render_template('/account/home.html', title='Account', image_file=image_file, form=form)
 
-    
+
+@app.route('/upload', methods=['GET', 'POST'])
+@login_required         
+def upload_image():
+    form = UploadPhotoForm()
+    if form.validate_on_submit():
+        album = Album(name = form.name.data, date = form.date.data, user_id=current_user.id)
+        db.session.add(album)
+        for file in form.image_file.data:
+            image = Picture(name = form.name.data, image_file = save_image(file), poster=album)
+            db.session.add(image)
+        db.session.commit()
+        flash('Photos have been uploaded!', 'success')
+        return redirect(url_for('upload_image'))
+    return render_template('upload.html', title='Upload Photos', form=form)
+
+def save_image(form_picture): #Move this into seperate file, along with db query/submit/connection stuff
+    filename = form_picture.filename
+    random_hex = binascii.b2a_hex(os.urandom(4)).decode("utf-8")
+    _, f_ext = os.path.splitext(filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/uploaded_pics', picture_fn)
+    i = Image.open(form_picture)
+    i.save(picture_path)
+    print("Save " + filename + " to " + picture_path, file=sys.stderr)
+    return picture_fn
+
+@app.route('/album/<int:album_id>')
+@login_required         
+def album(album_id):
+    photos = Picture.query.filter_by(album_id=album_id).all()
+    return render_template('album.html', title="Browse Album", photos = photos)
+
+# Delete all photos first, then album
+@app.route("/album/<int:album_id>/delete", methods=['GET'])
+@login_required
+def delete_album(album_id):
+    album = Album.query.get_or_404(album_id)
+    if album.user_id != current_user.id:
+        abort(403)
+    photos = Picture.query.filter_by(album_id=album_id).all()
+    for photo in photos:
+        db.session.delete(photo)
+    print(album, sep='\n', file=sys.stderr)
+    db.session.delete(album)
+    db.session.commit()
+    flash('Album has been deleted!', 'success')
+    return redirect(url_for('home_page'))
+
+# Delete photo, echk if album is empty, if so, delete album/
+@app.route("/album/<int:album_id>/<int:picture_id>/delete", methods=['GET'])
+@login_required
+def delete_picture(album_id, picture_id):
+    album = Album.query.get_or_404(album_id)                                # Get album object                            # Grab selected photo                                              # Store album id
+    if album.user_id != current_user.id:                                    # Check if user is allowed to access
+        abort(403)
+    photo = Picture.query.get_or_404(picture_id)
+    album_id = photo.album_id 
+    db.session.delete(photo)                                                # Delete
+    last_photo_check = Picture.query.filter_by(album_id=album_id).all()     # Check if photos exist with album id
+    if not last_photo_check:
+        db.session.delete(album)                                            # Delete album
+    db.session.commit() 
+    flash('Photo has been deleted!', 'success')
+    return redirect(url_for('browse_page'))
