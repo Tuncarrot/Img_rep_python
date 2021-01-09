8064f5d04147693df37262a3788030a846032880
Adam Tunkiewicz <Atunkiewicz0881@conestogac.on.ca>
2021-01-09 13:31:19 -0500

    :sparkles: add new album and picture model

--- imgRepFlask/models.py
+++ imgRepFlask/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from imgRepFlask import db, login_manager
+from imgRepFlask import login_manager, db
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -18,44 +18,37 @@ def load_user(user_id):
 #     def __repr__(self): # How our object is printed
 #         return 'User("{0}, {1}, {2}")'.format(self.name, self.email, self.image_file)
 
-# class Calorie(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     calorie = db.Column(db.Integer)
-#     processed_sugar =  db.Column(db.Integer)
-#     protein =  db.Column(db.Integer)
-#     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # Pass function as the argument, not include () because that would get time now
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-#     def __repr__(self): # How our object is printed
-#         return 'Calorie("Calories: {0}, Processed Sugar: {1},Protein: {2}, Date: {3}")'.format(self.calorie, self.processed_sugar, self.protein, self.date)
-
 class User(db.Model, UserMixin): #UserMixin?
     id = db.Column(db.Integer, primary_key=True)
-    contactInfo = db.relationship('ContactInfo', backref='user', uselist=False)
-    account = db.relationship('Account', backref='user', uselist=False) #uselist=false means 1-1 relationship
-    album = db.relationship('Album', backref='user', lazy=True)
+    contactInfo = db.relationship('ContactInfo', backref='creator', uselist=False)
+    account = db.relationship('Account', backref='creator', uselist=False) #uselist=false means 1-1 relationship
+    album = db.relationship('Album', backref='creator', lazy=True)
 
 class Account(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password =  db.Column(db.String(60), nullable = False) # 60 characters due to hash
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #profile pictures
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class ContactInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    date = db.Column(db.DateTime)
+    date = db.Column(db.String(10))
+    #date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    pictures = db.relationship('Picture', backref='album', lazy=True)
+    pictures = db.relationship('Picture', backref='poster', lazy=True)
+
+    def __repr__(self):
+        return str('Album {0}, {1}, {2}'.format(self.name, self.date, self.pictures))
 
 class Picture(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    date = db.Column(db.DateTime)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #profile pictures
     album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
